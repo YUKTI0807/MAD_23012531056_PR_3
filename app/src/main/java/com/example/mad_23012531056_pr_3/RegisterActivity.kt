fbc67cd60dcd59c8d3968c3c6e47af06447460b7
yukti <yuktisutariya06@gmail.com>
2025-09-28 19:00:52 +0530

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/mad_23012531056_pr_3/RegisterActivity.kt
+++ app/src/main/java/com/example/mad_23012531056_pr_3/RegisterActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mad_23012531056_pr_3
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,13 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        findViewById<Button>(R.id.btnlogin).setOnClickListener {
+            val loginintent = Intent(this, LoginActivity::class.java)
+            startActivity(loginintent)
+        }
+        findViewById<Button>(R.id.registerButton).setOnClickListener {
+            Toast.makeText(this, "Registered Successfully", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }

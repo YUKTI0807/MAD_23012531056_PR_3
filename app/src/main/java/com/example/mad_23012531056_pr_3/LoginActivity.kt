4efc9b1da8b264fd159a0e14fcfb4674c7d1aee5
yukti <yuktisutariya06@gmail.com>
2025-09-28 18:57:25 +0530

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/mad_23012531056_pr_3/LoginActivity.kt
+++ app/src/main/java/com/example/mad_23012531056_pr_3/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mad_23012531056_pr_3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
